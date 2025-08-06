c5c9a6ee0b6d30c5493f514cfe0ce37279f44282
jayasurya <jayasurya@sparkouttech.com>
2025-08-06 22:06:29 +0530

    feat : sample api route implemented

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World from Node.js!');
+});
+
+let user = [];
+
+app.post('/user', (req, res) => {
+  const { name, time } = req.body;
+
+  if (!name || !time) {
+    return res.status(400).json({ error: "Missing 'name' or 'time'" });
+  }
+
+  const newComment = { name, time };
+  user.push(newComment);
+
+  res.status(201).json({ message: "Comment added", comment: newComment });
 });
 
 app.listen(port, () => {
